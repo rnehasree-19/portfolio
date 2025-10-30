c0d9b031405c8a41c13cb228fa2660a6e25eff66
R Neha Sree <rnehasree@gmail.com>
2025-10-31 00:05:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let colors = [
+    "#1B263B",  // Midnight Blue
+    "#415A77",  // Steel Blue
+    "#778DA9",  // Light Slate Blue
+    "#A8DADC",  // Pale Cyan Blue
+    "#E0FBFC",  // Light Cyan (almost white)
+    "#98C1D9",  // Soft Sky Blue
+    "#3D5A80"   // Dark Teal Blue
+];
+let i = 0;
+
+document.onmousemove = function(e) {
+    i++;
+    let x = e.pageX;
+    let y = e.pageY;
+
+    let span }
